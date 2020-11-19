2378e25360aa97f99a2cfce70ee181360ce3c121
benoit barges <benoitbarges@gmail.com>
2020-11-19 10:57:51 +0100

    subscribe

--- store/index.js
+++ store/index.js
@@ -1,7 +1,22 @@
 function createStore () {
+  // The store should have 4 parts
+  // 1. The state
+  // 2. Get the state
+  // 3. Listen to changes on the state
+  // 4. Update the state
+
   let state
+  let listeners = []
 
   const getState = () => state
+
+  const subscribe = (listener) => {
+    listeners.push(listener)
+
+    return () => {
+      listeners = listeners.filter(l => l !== listener)
+    }
+  }
 
   return {
     getState
